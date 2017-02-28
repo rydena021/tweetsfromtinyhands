fb1aaf4e266be9658aa85b6a1d07c2b96b32c66c
John <giovannibitar@gmail.com>
2017-02-28 17:43:39 +0100

    I hope this works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jsonfile = File.open('db/tweets.json').read
+parsed = JSON.parse(jsonfile)
+parsed.each do |hash|
+  Tweet.create(in_reply_to_screen_name: hash["in_reply_to_screen_name"],
+    twitter_id: hash["id_str"], source: hash["source"], retweet_count: hash["retweet_count"],
+    text: hash["text"], posted_at: hash["created_at"], is_retweet: hash["is_retweet"],
+    favorite_count: hash["favorite_count"])
+end
