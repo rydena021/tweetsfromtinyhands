965e5d64b90759e5c5c80864eaeb7536bea88d5d
Anders Ryden <rydena021@gmail.com>
2017-02-28 19:25:06 +0100

    creates practice view, implements tooltip

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#design'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/components', to: 'pages#components'
 end
