676800bc0ac5baf865dfa72e7798876714e193a9
Nicolas Feer <atmosfeer@gmail.com>
2017-03-09 18:54:15 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#components'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/components', to: 'pages#components'
   get '/sentiment', to: 'pages#sentiment'
