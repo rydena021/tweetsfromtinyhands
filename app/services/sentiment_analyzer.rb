a89907b1a32fd965a72ec786f88df7e560c5c242
John <giovannibitar@gmail.com>
2017-03-09 15:57:44 +0100

    yay

--- app/services/sentiment_analyzer.rb
+++ app/services/sentiment_analyzer.rb
@@ -20,18 +20,17 @@ class SentimentAnalyzer
 
   def self.json_write(array, date)
     sentiment_hash = {
-      positive: 0
-      negative: 0
+      positive: 0,
+      negative: 0,
       neutral: 0
     }
     array.each do |tweet|
-      sentiment_hash[:positive] += 1 if tweet.sentiment = positive
-      sentiment_hash[:negative] += 1 if tweet.sentiment = negative
-      sentiment_hash[:neutral] += 1 if tweet.sentiment = neutral
+      sentiment_hash[:positive] += 1 if tweet.sentiment == "positive"
+      sentiment_hash[:negative] += 1 if tweet.sentiment == "negative"
+      sentiment_hash[:neutral] += 1 if tweet.sentiment == "neutral"
     end
-        }
     File.open("app/assets/data/sentiment_jsons/#{date.to_date.to_s}.json", 'wb') do |file|
-      file.write(JSON.generate(word_hash))
+      file.write(JSON.generate(sentiment_hash))
     end
   end
 
@@ -40,7 +39,8 @@ class SentimentAnalyzer
     start_date = DateTime.parse("2015-06-16").beginning_of_month
     end_date = DateTime.current.beginning_of_month
 
-    date_months = (start_date..end_date).map { |d| DateTime.new(d.year, d.month, 1)  }.uniq
+    date_months = (start_date..end_date).map { |d| DateTime.new(d.year, d.month, 1) }
+    date_months.uniq
     date_months.each_with_index do |date, index|
       range = date..(date_months[index + 1] || end_date)
         SentimentAnalyzer.json_write(Tweet.where(posted_at: range), date)
