e2cdbef73f1f816a02461e9db0993e5cd76614af
Anders Ryden <rydena021@gmail.com>
2017-03-16 09:43:15 -0500

    .js production debug

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module RailsTinyHands
     config.generators do |generate|
       generate.assets true
     end
-
+config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
