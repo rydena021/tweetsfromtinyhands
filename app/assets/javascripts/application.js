d3b10122db66355edd12a595f1fd5934311e20dc
Anders Ryden <rydena021@gmail.com>
2017-03-29 07:00:24 -0500

    parallax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,11 @@
 
 
 // jQuery materialize plugins
-$(".parallax").parallax();
-$(".button-collapse").sideNav();
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
